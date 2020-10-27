22cb88625cb0d637e5cf3157c720b4f312a1ad65
Karansutradhar <kssutrad@terpmail.umd.edu>
2020-10-27 18:37:33 -0400

    updated files with cpplint and cppcheck errors

--- beginner_tutorials/src/talker.cpp
+++ beginner_tutorials/src/talker.cpp
@@ -1,13 +1,20 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+/**
+ * @file talker.cpp
+ * @author Karan Sutradhar
+ * @brief The talker.cpp file for beginner_tutorials ROS package.
+ * It contains code for publishing a simple message.
+ * @Copyright "Copyright 2020" <Karan Sutradhar>
+ */
 
 #include <sstream>
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -53,8 +60,7 @@ int main(int argc, char **argv)
    * a unique string for each message.
    */
   int count = 0;
-  while (ros::ok())
-  {
+  while (ros::ok()) {
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
